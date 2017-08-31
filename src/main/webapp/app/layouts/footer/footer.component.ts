f255122edc1f14c0cd14bf99d39702032b2083b7
gregory <boeroteyssier.gregory@gmail.com>
2017-08-31 23:14:17 +0200

    set the new footer

--- src/main/webapp/app/layouts/footer/footer.component.ts
+++ src/main/webapp/app/layouts/footer/footer.component.ts
@@ -1,7 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {VERSION, DEBUG_INFO_ENABLED} from '../../app.constants';
 
 @Component({
     selector: 'jhi-footer',
-    templateUrl: './footer.component.html'
+    templateUrl: './footer.component.html',
+    styleUrls: [
+        'footer.css'
+    ]
 })
-export class FooterComponent {}
+export class FooterComponent {
+    version: string;
+
+    constructor() {
+        this.version = VERSION ? 'v' + VERSION : '';
+    }
+}
